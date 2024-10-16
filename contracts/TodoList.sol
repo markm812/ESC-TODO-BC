b647807c7c95e01397b62fdaac2b0cbabf3a6ecb
Mark.Ma <markma08181212@gmail.com>
2024-10-16 14:59:01 -0400

    Add Task struct and createTask function to TodoList contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+        uint creationDate;
+    }
+
+    // Hash table to store tasks
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Try adding a task to the list");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false, block.timestamp);
+    }
 }
